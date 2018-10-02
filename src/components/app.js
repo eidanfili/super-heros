325bd83000c7db20df41b0d083ca3126bf29471c
eidanfili <eidan@ledezma.org>
2018-10-02 10:21:37 -0600

    work please

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Supers from './supers';
+
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Supers name="Man"/>
+        <Supers name="Joh"/>
+        <Supers name="Hegery"/>
+      </div>
+      
+    )
+  }
+}
---
+++ src/components/supers.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+function Supers(props) {
+        return <h2>{props.name}</h2>
+}
+
+export default Supers;
